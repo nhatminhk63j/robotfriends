8aaca9007a5c391a05e7dc2beff9c35ecdd7f84b
Nhat Ngo Sach <nhatngo11a1@gmail.com>
2020-01-30 22:44:08 +0700

    Updates

---
+++ precache-manifest.4b856e80c90c4335a02a8bf69865686e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "65b710121aefcd1263a9de3d3c7235da",
+    "url": "/robotfriends/index.html"
+  },
+  {
+    "revision": "75d825fd6545a95b2efc",
+    "url": "/robotfriends/static/css/main.135bea0c.chunk.css"
+  },
+  {
+    "revision": "1b27418fb72069d14475",
+    "url": "/robotfriends/static/js/2.a1a4a5a8.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/robotfriends/static/js/2.a1a4a5a8.chunk.js.LICENSE"
+  },
+  {
+    "revision": "75d825fd6545a95b2efc",
+    "url": "/robotfriends/static/js/main.e22aab4e.chunk.js"
+  },
+  {
+    "revision": "8432a6681c6fc45d179d",
+    "url": "/robotfriends/static/js/runtime-main.05482e96.js"
+  }
+]);
